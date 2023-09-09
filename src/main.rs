5c2c0b6b49c93f14ef5e809e8a8116fea4fa9015
Pena <33009908+fpena2@users.noreply.github.com>
2023-09-09 00:15:12 -0500

    Experimenting with modules

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,14 @@
-use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
+use actix_web::{web, App, HttpServer};
 use std::sync::Mutex;
-
-struct World {
-    resources: Mutex<u64>,
-}
-
-#[derive(Deserialize)]
-struct Worker {
-    uid: String,
-    work: u64,
-    // Credit for work that was not accepted
-    worker_credit: u64,
-}
-
-#[derive(Serialize)]
-struct WorkResponse {
-    mine_resources: u64,
-    work_credit: u64,
-}
-
-#[post("/do_work")]
-async fn do_work(info: web::Json<Worker>, data: web::Data<World>) -> impl Responder {
-    let mut shared_data = data.resources.lock().unwrap();
-    let mut credit = 0;
-
-    if let Some(result) = shared_data.checked_sub(info.work) {
-        println!("Your work has been processed: {}!", info.work);
-        *shared_data = result;
-    } else {
-        println!("You work was partially processed!");
-        if let Some(c) = info.work.checked_sub(*shared_data) {
-            // TODO: this breaks integrity since workers can provide their own credit
-            credit = c + info.worker_credit;
-            *shared_data = 0;
-        } else {
-            todo!()
-        };
-    }
-
-    let response = WorkResponse {
-        mine_resources: *shared_data,
-        work_credit: credit,
-    };
-
-    HttpResponse::Ok().json(response)
-}
+pub mod module;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mine = web::Data::new(World {
+    let mine = web::Data::new(module::World {
         resources: Mutex::new(3),
     });
 
-    HttpServer::new(move || App::new().app_data(mine.clone()).service(do_work))
+    HttpServer::new(move || App::new().app_data(mine.clone()).service(module::do_work))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
---
+++ src/module.rs
@@ -0,0 +1,50 @@
+use actix_web::{post, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+
+pub struct World {
+    pub resources: Mutex<u64>,
+}
+
+#[derive(Deserialize)]
+pub struct Worker {
+    pub uid: String,
+    pub work: u64,
+    // Credit for work that was not accepted
+    pub worker_credit: u64,
+}
+
+#[derive(Serialize)]
+pub struct WorkResponse {
+    pub mine_resources: u64,
+    pub work_credit: u64,
+}
+
+#[post("/do_work")]
+pub async fn do_work(info: web::Json<Worker>, data: web::Data<World>) -> impl Responder {
+    let mut shared_data = data.resources.lock().unwrap();
+    let mut credit = 0;
+
+    println!("Hello: {}!", info.uid);
+
+    if let Some(result) = shared_data.checked_sub(info.work) {
+        println!("Your work has been processed: {}!", info.work);
+        *shared_data = result;
+    } else {
+        println!("You work was partially processed!");
+        if let Some(c) = info.work.checked_sub(*shared_data) {
+            // TODO: this breaks integrity since workers can provide their own credit
+            credit = c + info.worker_credit;
+            *shared_data = 0;
+        } else {
+            todo!()
+        };
+    }
+
+    let response = WorkResponse {
+        mine_resources: *shared_data,
+        work_credit: credit,
+    };
+
+    HttpResponse::Ok().json(response)
+}
